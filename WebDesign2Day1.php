72cb9c876ed57260b851b7f728185d74d2fa21bd
sachoco <sachoco@gmail.com>
2013-09-28 18:27:47 +0200

    Connecting to mySQL

--- WebDesign2Day1.php
+++ WebDesign2Day1.php
@@ -7,17 +7,25 @@
 	<p>
 <?php
 
-/* 	$_GET variable get values from url e.g. filename.php?satoshi=happy */
-	$satoshi = $_GET["satoshi"];
+	$username = "your_name";
+	$password = "your_password";
+	$hostname = "localhost"; 
 	
-/* 	Let's user the "IF" statement */
-	if($satoshi=="happy"){
-		echo "He is jumping around in the class."
-	}else{
-		echo "He doesn't talk."
+	//connection to the database
+	$dbhandle = mysql_connect($hostname, $username, $password) 
+	  or die("Unable to connect to MySQL");
+
+	//select a database to work with
+	$selected = mysql_select_db("day1",$dbhandle) 
+	  or die("Could not select examples");
+
+	//execute the SQL query and return records
+	$result = mysql_query("SELECT * FROM students");
+	//fetch tha data from the database
+	while ($row = mysql_fetch_array($result)) {
+	   echo $row['first_name']." ".$row['last_name']." from ".$row['country']."<br>";
 	}
-	
-/* 	Try to see which line of aboves shows up */
+
 ?>
 	</p>
 </body>
