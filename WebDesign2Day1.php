13799d939b720dc59331e63bc3f67e84d1c4a91b
sachoco <sachoco@gmail.com>
2013-09-28 18:05:02 +0200

    Basic of PHP (1~3)

---
+++ WebDesign2Day1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Introduction to PHP</title>
+</head>
+<body>
+	<h1>Introduction to PHP</h1>
+	<p>
+<?php
+	echo "echo is a php function to print text (string) on the (html) document";
+?>
+	</p>
+</body>
+</html>
